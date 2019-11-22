47d9cbbcf5e94012c4c5d16d7482fe5dd95cc5ab
Zhdanov <evdev34@gmail.com>
2019-11-22 18:10:06 +0300

    bundle gem

---
+++ lib/rucaptcha_solve.rb
@@ -0,0 +1,6 @@
+require "rucaptcha_solve/version"
+
+module RucaptchaSolve
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rucaptcha_solve/version.rb
@@ -0,0 +1,3 @@
+module RucaptchaSolve
+  VERSION = "0.1.0"
+end
